c3314a5f061426ee5618205fc208861c0d0f8dd7
Tim <tim@tim.tim>
2023-09-22 12:09:09 +0200

    Initial commit

---
+++ Robofight/Robot.cs
@@ -0,0 +1,15 @@
+﻿namespace Robofight;
+
+public class Robot
+{
+    public string Name { get; set; }
+    
+    public int HealthPoints { get; set; }
+    
+    public int Damge { get; set; }
+
+    public void Attack(Robot opponent)
+    {
+        opponent.HealthPoints -= Damge;
+    }
+}
